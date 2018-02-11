7664e58569c3baa44c7ef6e151e525f588f56d64
iamaCookie <iamaCookie321@outlook.com>
2018-02-11 13:31:00 -0600

    pdf

--- WIP/Chris/mail/__init__.py
+++ WIP/Chris/mail/__init__.py
@@ -16,8 +16,8 @@ import time
 
 # DYNAMIC SECTION
 first_name = "JOHN"
-last_name = "JOHNSON"
-address_1 = "2 Shiter St."
+last_name = "Hilker"
+address_1 = "2 Market St."
 address_2 = "Saint Louis, MO 63110"
 DATES=["1/2/14","1/4/15","4/11/16","5/7/20","5/3/4"]
 ITEMS=["Sweater","Boots","Shirts","Jacket","ITEM"]
